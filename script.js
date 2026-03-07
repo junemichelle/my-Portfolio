641112fccf9c6b8c7d592958d605526fd840c4ab
junemichelle <junemishelle20@gmail.com>
2026-03-08 01:40:14 +0300

    index.html

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a')
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
-
-window.onscroll = ()=>{
-  sections.forEach(sec => {
+window.onscroll = () => {
+  sections.forEach((sec) => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
+    let id = sec.getAttribute("id");
 
-    if(top >= offset && top < offset + height){
-      navLinks.forEach(links =>{
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
         links.classList.remove("active");
-        document.querySelector('header nav a [href*='+id+']').classList.add
-        ('active')
-      })
+        document
+          .querySelector("header nav a [href*=" + id + "]")
+          .classList.add("active");
+      });
     }
-  })
-}
+  });
+};
 
-menuIcon.onclick = ()=>{
-  menuIcon.classList.toggle('bx-x');
-  navbar.classList.toggle('active');
-}
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
 const form = document.getElementById("contact-form");
 const thankYou = document.getElementById("thank-you");
 
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   const data = new URLSearchParams(new FormData(form)).toString();
@@ -37,11 +37,11 @@ form.addEventListener("submit", function(event) {
   fetch("/", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: data
+    body: data,
   })
-  .then(() => {
-    form.style.display = "none";
-    thankYou.style.display = "block";
-  })
-  .catch((error) => alert("Form submission failed: " + error));
+    .then(() => {
+      form.style.display = "none";
+      thankYou.style.display = "block";
+    })
+    .catch((error) => alert("Form submission failed: " + error));
 });
